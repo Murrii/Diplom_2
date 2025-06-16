cac8acf8a4a70fdf587930e4a266f33107eebf5d
Karina Trofimova <karina_seidova@mail.ru>
2025-06-16 19:08:38 +0200

    Добавлены тесты для ручки /users. Удалены методы /logout (в тестах заменены созданием нового пользователя с последующим удалением)

--- data.py
+++ data.py
@@ -14,6 +14,11 @@ LOGIN_ERROR_NOT_VALID_DATA = "email or password are incorrect"
 
 USER_NOT_AUTH_CHANGE = "You should be authorised"
 
+CREATE_ORDER_WITHOUT_INGREDIENTS = "Ingredient ids must be provided"
+CREATE_ORDER_WITH_WRONG_INGREDIENT_ID = "One or more ids provided are incorrect"
+
+GET_ORDER_LIST_WITHOUT_AUTH = "You should be authorised"
+
 # данные существующего пользователя для тестов авторизации с невалидными данными
 LOGIN_DATA_WRONG_EMAIL = {
 "email": "wrong_ktrof_006@yandex.ru",
@@ -31,3 +36,12 @@ CHANGE_DATA ="changed_ktrof_012@mail.ru"
 # хеши ингредиентов для теста заказов
 INGREDIENT_BUN_ID = "61c0c5a71d1f82001bdaaa6d"
 INGREDIENT_MAIN_ID = "61c0c5a71d1f82001bdaaa71"
+INGREDIENT_SAUCE_ID = "61c0c5a71d1f82001bdaaa72"
+INGREDIENT_WRONG_ID = "61c0c5a71d1f82001bdaaa64"
+
+# Данные пользователя для получения заказов пользователя
+
+USER_WITH_ORDERS_DATA = {
+"email": "ktrof_006@yandex.ru",
+"password": "ktrof_006@yandex.ru"
+}
--- methods/order_methods.py
+++ methods/order_methods.py
@@ -1,3 +1,4 @@
+import allure
 import requests
 from data import BASE_URL, ORDER_URL
 
@@ -5,10 +6,22 @@ class OrderMethods:
     def __init__(self):
         self.url = BASE_URL + ORDER_URL
 
+    @allure.step("Создаем заказ без авторизации")
     def create_order_without_auth(self, ingredients):
         response = requests.post(url=self.url, data={"ingredients": ingredients})
         return response.status_code, response.json()
 
+    @allure.step("Создаем заказ с авторизацией")
     def create_order_with_auth(self, access_token, ingredients):
         response = requests.post(url=self.url, headers={"authorization": access_token}, data={"ingredients": ingredients})
+        return response.status_code, response.json()
+
+    @allure.step("Получаем список заказов пользователя с авторизацией")
+    def get_orders_list_with_auth(self, access_token):
+        response = requests.get(url=self.url, headers={"authorization": access_token})
+        return response.status_code, response.json()
+
+    @allure.step("Получаем список заказов пользователя без авторизации")
+    def get_orders_list_without_auth(self):
+        response = requests.get(url=self.url)
         return response.status_code, response.json()
